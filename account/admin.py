647c3eb706ca76cffa19c1b70489d7902b1f81e2
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-05-05 11:28:39 +0300

    Add AdminModels

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-
-from .models import Employee, Owner
-
-
-admin.site.register([Employee, Owner])
--- lunch/admin.py
+++ lunch/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.db.models import Count
+from .models import Restaurant, Menu
 
-from .models import Restaurant, DailyMenu
+admin.site.register(Restaurant)
 
 
-admin.site.register([Restaurant, DailyMenu])
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['restaurant_name', 'votes']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(votes=Count('customers_vote')).order_by('-votes')
+        return qs
+
+    def restaurant_name(self, obj):
+        return obj.restaurant.name
+
+    def votes(self, obj):
+        return obj.votes
